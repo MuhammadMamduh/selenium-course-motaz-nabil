046052e472fa38866bb714fbdf4702468f1de1ea
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2024-03-13 21:10:38 +0200

    [FIX OUTDATED CODE][UserRegistrationWithDDTAndDataProvider.java]

--- src/test/java/tests/UserRegistrationWithDDTAndDataProvider.java
+++ src/test/java/tests/UserRegistrationWithDDTAndDataProvider.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.UserRegistrationPage;
+import utilities.Helper;
 
 public class UserRegistrationWithDDTAndDataProvider extends TestBase
 {
@@ -18,22 +19,30 @@ public class UserRegistrationWithDDTAndDataProvider extends TestBase
 
 
 	@DataProvider(name="testData")
-	public static Object[][] userData()
+	public static Object[][] userData() throws InterruptedException
 	{
+		String emailString_1 = Helper.getUniqueEmail();
+		Thread.sleep(1000);
+		String emailString_2 = Helper.getUniqueEmail();
 		return new Object[][] {
-			{"Moataz" , "Nabil","testxxx996@gmail.com","123456"},
-			{"Ahmed","Ali","testuser1270073@gmail.com","12345678"}
+			{"Moataz" , "Nabil",emailString_1,"123456"},
+			{"Ahmed","Ali",emailString_2,"12345678"}
 		};
 	}
 
 	@Test(priority=1,dataProvider="testData")
 	public void UserCanRegisterSuccssfully(String fname, String lname , String email , String password ) 
 	{
-		homeObject = new HomePage(driver); 
+		homeObject = new HomePage(driver);
+		loginObject = new LoginPage(driver);
 		homeObject.openRegistrationPage();
 		registerObject = new UserRegistrationPage(driver); 
 		registerObject.userRegistration(fname,lname,email,password);
 		Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
+		
+		homeObject.openLoginPage();
+		loginObject.UserLogin(email, password);
+		
 		registerObject.userLogout();
 		homeObject.openLoginPage();
 		loginObject = new LoginPage(driver); 
