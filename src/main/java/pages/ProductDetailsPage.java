08bb9c9d8113b5561a36be8ef21091b54622d235
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2024-03-14 22:47:39 +0200

    [FIX OUTDATED CODE][AddProductToWishlist][wait until the confirmation msg appears]

--- src/main/java/pages/ProductDetailsPage.java
+++ src/main/java/pages/ProductDetailsPage.java
@@ -1,13 +1,21 @@
 package pages;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ProductDetailsPage extends PageBase
 {
+	public static WebDriverWait wait;
+
 	public ProductDetailsPage(WebDriver driver) {
 		super(driver);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
 	@FindBy(css="strong.current-item")
@@ -16,8 +24,8 @@ public class ProductDetailsPage extends PageBase
 	@FindBy(css="button.button-2.email-a-friend-button")
 	WebElement emailFriendBtn ; 
 
-	@FindBy(css="span.price-value-4")
-	public WebElement productPricelbl ;
+	@FindBy(id="price-value-4")
+	public WebElement productPricelbl;
 
 	@FindBy(id="add-to-wishlist-button-4")
 	WebElement addToWishlistBtn ; 
@@ -26,10 +34,13 @@ public class ProductDetailsPage extends PageBase
 	WebElement addReviewLink; 
 
 	@FindBy(xpath="//div[@class='overview-buttons']//child::button[contains(text(), 'Add to compare list')]")
-	WebElement addToCompareBtn ; 
+	WebElement addToCompareBtn; 
 	
-	@FindBy(id="add-to-cart-button-4")
-	WebElement addToCartBtn ; 
+	@FindBy(css="button.button-1.add-to-cart-button")
+	WebElement addToCartBtn;
+
+	@FindBy(xpath="//*[@id='bar-notification']//child::a[contains(text(), 'wishlist')]")
+	public WebElement addedToWishListSuccessMsg;
 
 	public void openSendEmail() 
 	{
@@ -44,6 +55,7 @@ public class ProductDetailsPage extends PageBase
 	public void AddProductToWishlist() 
 	{
 		clickButton(addToWishlistBtn);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='bar-notification']//child::a[contains(text(), 'wishlist')]")));
 	}
 
 	public void AddProductToCompare() 
