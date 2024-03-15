20d1ac0cd1fd74ee1ed21f75bd0c126622976a75
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2024-03-15 17:14:40 +0200

    [FIX OUTDATED CODE][Loading from properties file]

--- src/test/java/data/LoadProperties.java
+++ src/test/java/data/LoadProperties.java
@@ -2,14 +2,18 @@ package data;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
+
+import utilities.Helper;
 
 public class LoadProperties 
 {
 	// Load the properties file from the folder
 	public static Properties userData =  
-			loadProperties(System.getProperty("user.dir")+"\\src\\main\\java\\properties\\userdata.properties"); 
+			loadProperties(System.getProperty("user.dir") + "/src/main/java/properties/userdata.properties"); 
 	
 	
 	private static Properties loadProperties(String path)
@@ -19,6 +23,11 @@ public class LoadProperties
 		try {
 			FileInputStream stream = new FileInputStream(path);
 			pro.load(stream);
+			pro.setProperty("email", Helper.getUniqueEmail());
+			
+			// *** OPTIONAL *** If you want to update the properties file also
+			OutputStream output = new FileOutputStream(path);
+            pro.store(output, null);
 		} catch (FileNotFoundException e) {
 		System.out.println("Error occurred :  " + e.getMessage());
 		} catch (IOException e) {
--- src/test/java/tests/UserRegistrationWithDDTAndPropertiesFile.java
+++ src/test/java/tests/UserRegistrationWithDDTAndPropertiesFile.java
@@ -24,26 +24,33 @@ public class UserRegistrationWithDDTAndPropertiesFile extends TestBase
 	@Test(priority=1,alwaysRun=true)
 	public void UserCanRegisterSuccssfully() 
 	{
-	
+		System.out.println("============================");
+		System.out.println("============================");
+		System.out.println(firtname);
+		System.out.println(lastname);
+		System.out.println(email);
+		System.out.println(Password);
+		System.out.println("============================");
+		System.out.println("============================");
 		homeObject = new HomePage(driver); 
 		homeObject.openRegistrationPage();
-		registerObject = new UserRegistrationPage(driver); 
+		registerObject = new UserRegistrationPage(driver);
 		registerObject.userRegistration(firtname,lastname,email,Password);
 		Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
 	}
 	
 	@Test(dependsOnMethods= {"UserCanRegisterSuccssfully"})
-	public void RegisteredUserCanLogout() 
-	{
-		registerObject.userLogout();
-	}
-	
-	@Test(dependsOnMethods= {"RegisteredUserCanLogout"})
 	public void RegisteredUserCanLogin() 
 	{
 		homeObject.openLoginPage();
 		loginObject = new LoginPage(driver); 
 		loginObject.UserLogin(email,Password);
 		Assert.assertTrue(registerObject.logoutLink.getText().contains("Log out"));
+	}
+
+	@Test(dependsOnMethods= {"RegisteredUserCanLogin"})
+	public void RegisteredUserCanLogout() 
+	{
+		registerObject.userLogout();
 	}
 }
