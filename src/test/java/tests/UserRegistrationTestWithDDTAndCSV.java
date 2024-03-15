99e72aa8492326febbbe7c582ff45d9c462f9739
Muhammad_Mamduh <muhammad.mma.oz@gmail.com>
2024-03-15 23:33:27 +0200

    [FIX OUTDATED CODE][Loading from CSV]

--- src/test/java/tests/UserRegistrationTestWithDDTAndCSV.java
+++ src/test/java/tests/UserRegistrationTestWithDDTAndCSV.java
@@ -3,16 +3,19 @@ package tests;
 
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
 
 import pages.HomePage;
 import pages.LoginPage;
 import pages.UserRegistrationPage;
+import utilities.Helper;
 
 public class UserRegistrationTestWithDDTAndCSV extends TestBase
 {
@@ -20,42 +23,49 @@ public class UserRegistrationTestWithDDTAndCSV extends TestBase
 	UserRegistrationPage registerObject ; 
 	LoginPage loginObject ; 
 
-	CSVReader reader ; 
+	CSVReader reader ;
+	CSVWriter writer ;
 
 	@Test(priority=1,alwaysRun=true)
 	public void UserCanRegisterSuccssfully() throws IOException 
 	{
 		// get path of CSV file 
-		String CSV_file = System.getProperty("user.dir")+"/src/test/java/data/UserData.csv";
+		String CSV_file = System.getProperty("user.dir") + "/src/test/java/data/UserData.csv";
 		reader = new CSVReader(new FileReader(CSV_file)); 
-
 		String[] csvCell ; 
 
-		try {
-		// while loop will be executed till the lastvalue in CSV file . 
-		while((csvCell = reader.readNext()) != null) 
+		try 
 		{
-			String firstname = csvCell[0]; 
-			String lastName = csvCell[1]; 
-			String email = csvCell[2]; 
-			String password = csvCell[3]; 
+			// while loop will be executed till the lastvalue in CSV file . 
+			while((csvCell = reader.readNext()) != null) 
+			{
+				String firstname = csvCell[0]; 
+				String lastName = csvCell[1];
+				String email = csvCell[2];
 
-			homeObject = new HomePage(driver); 
-			homeObject.openRegistrationPage();
-			registerObject = new UserRegistrationPage(driver); 
-			registerObject.userRegistration(firstname, lastName, email, password);
-			Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
-			registerObject.userLogout();
-			homeObject.openLoginPage();
-			loginObject = new LoginPage(driver); 
-			loginObject.UserLogin(email,password);
-			Assert.assertTrue(registerObject.logoutLink.getText().contains("Log out"));
-			registerObject.userLogout();
+				int index = email.indexOf("@");
+				email = email.substring(0, index) +
+						System.currentTimeMillis() +
+						email.substring(index);
+
+				String password = csvCell[3]; 
+
+				homeObject = new HomePage(driver); 
+				homeObject.openRegistrationPage();
+				registerObject = new UserRegistrationPage(driver); 
+				registerObject.userRegistration(firstname, lastName, email, password);
+				Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
+				// registerObject.userLogout();
+				homeObject.openLoginPage();
+				loginObject = new LoginPage(driver); 
+				loginObject.UserLogin(email,password);
+				Assert.assertTrue(registerObject.logoutLink.getText().contains("Log out"));
+				registerObject.userLogout();
+			}
 		}
-	}
-	catch (Exception e)
-	{
-		System.out.println("Exception: " + e.getMessage());
-	}
+		catch (Exception e)
+		{
+			System.out.println("Exceptionnnnnnnnnnnnn: " + e.getMessage());
+		}
 	}
 }
